25257cadfe3b8574201a78ac59428e9bda360c85
Günther Jena <guenther@jena.at>
2021-12-20 20:45:16 +0100

    wip

--- megacard_lcd.c
+++ megacard_lcd.c
@@ -1,39 +1,3 @@
-/*****************************************************************************
- 
- megacard_lcd.c - LCD over I2C library for HTL Rankweil Megacard 
-		Designed for HD44870 based LCDs with I2C expander PCF8574X
-		on Atmels AVR MCUs
- 
- Copyright (C) 2006 Nico Eichelmann and Thomas Eichelmann
-               2014 clean up by Falk Brunner 
-			   2021 adaptions for HTL Rankweil Megacard by Günther Jena
-
-               Includes I2C library by Peter Fleury <pfleury@gmx.ch>  http://jump.to/fleury
- 
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.
- 
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Lesser General Public License for more details.
- 
- You should have received a copy of the GNU Lesser General Public
- License along with this library; if not, write to the Free Software
- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- 
- You can contact the authors at info@computerheld.de
- 
-*****************************************************************************/
-
-/*
-	Version 0.11
-	Requires I2C-Library from Peter Fleury http://jump.to/fleury
-	
-	See i2clcd.h for description and example.
-*/
 #define F_CPU 12000000UL       // CPU clock for Megacard is 12MHz
 
 #include <avr/pgmspace.h>
@@ -44,25 +8,127 @@
 #include <compat/twi.h>
 #include "megacard_lcd.h"
 
-#define SCL_CLOCK  100000L
+#define I2C_READ    1
+#define I2C_WRITE   0
 
-/*************************************************************************
- Initialization of the I2C bus interface. Need to be called only once
-*************************************************************************/
-void i2c_init(void)
-{
-  /* initialize TWI clock: 100 kHz clock, TWPS = 0 => prescaler = 1 */
-  
-  TWSR = 0;                         /* no prescaler */
-  TWBR = ((F_CPU/SCL_CLOCK)-16)/2;  /* must be > 10 for stable operation */
+#define LCD_I2C_DEVICE		0x4E	    /**< Change this to the address of your expander */
+#define LCD_LINES			2	        /**< Enter the number of lines of your display here */
+#define LCD_COLS			16	        /**< Enter the number of columns of your display here */
+#define LCD_LINE_MODE       LCD_2LINE   /**< Enter line mode your display here */
 
-}/* i2c_init */
+#define LCD_LINE1			0x00	    /**< This should be 0x00 on all displays */
+#define LCD_LINE2			0x40	    /**< Change this to the address for line 2 on your display */
+//#define LCD_LINE3			0x40	    /**< Change this to the address for line 3 on your display */
+//#define LCD_LINE4			0x60	    /**< Change this to the address for line 4 on your display */
+/*@}*/
 
+#if LCD_LINES > 4
+    #error "#define LCD_LINES must be less or equal to 4"
+#endif
 
-/*************************************************************************	
-  Issues a start condition and sends address and transfer direction.
-  return 0 = device accessible, 1= failed to access device
-*************************************************************************/
+#if LCD_COLS > 20
+    #error "#define LCD_COLS must be less or equal to 20"
+#endif
+
+#define LCD_D4_PIN			4	/**< LCD-Pin D4 is connected to P4 on the PCF8574 */
+#define LCD_D5_PIN			5	/**< LCD-Pin D5 is connected to P5 on the PCF8574 */
+#define LCD_D6_PIN			6	/**< LCD-Pin D6 is connected to P6 on the PCF8574 */
+#define LCD_D7_PIN			7	/**< LCD-Pin D7 is connected to P7 on the PCF8574 */
+#define LCD_RS_PIN			0	/**< LCD-Pin RS is connected to P0 on the PCF8574 */
+#define LCD_RW_PIN			1	/**< LCD-Pin RW is connected to P1 on the PCF8574 */
+#define LCD_E_PIN			  2	/**< LCD-Pin E is connected to P2 on the PCF8574 */
+#define LCD_LIGHT_PIN		3	/**< LCD backlight is connected to P3 on the PCF8574, low active */
+
+#define LCD_D4				(1 << LCD_D4_PIN)	/**< bit 4 in 2nd lower nibble */
+#define LCD_D5				(1 << LCD_D5_PIN)	/**< bit 5 in 2nd lower nibble */
+#define LCD_D6				(1 << LCD_D6_PIN)	/**< bit 6 in 2nd lower nibble */
+#define LCD_D7				(1 << LCD_D7_PIN)	/**< bit 7 in 2nd lower nibble */
+
+#define LCD_RS				(1 << LCD_RS_PIN)	/**< RS-bit in 1st and 2nd higher nibble */
+#define LCD_RW				(1 << LCD_RW_PIN)	/**< RW-bit in 1st and 2nd higher nibble */
+#define LCD_LIGHT_N			(1 << LCD_LIGHT_PIN)/**< LCD backlight control, low active */
+#define LCD_E				(1 << LCD_E_PIN)	/**< E-bit in 1st and 2nd higher nibble */
+
+/*@}*/
+
+// data & control bits for internal use, do not change!
+
+#define CMD_D0				(1 << 0)	/**< bit 0 in lower nibble */
+#define CMD_D1				(1 << 1)	/**< bit 1 in lower nibble */
+#define CMD_D2				(1 << 2)	/**< bit 2 in lower nibble */
+#define CMD_D3				(1 << 3)	/**< bit 3 in lower nibble */
+#define CMD_RS				(1 << 4)	/**< RS-bit */
+#define CMD_RW				(1 << 5)	/**< RW-bit */
+
+/** \defgroup DEFINED_READ_MODES DEFINED READ MODES
+*/
+/*@{*/
+#define LCD_ADDRESS			0	/**< Used for reading the address-counter and busy-flag */
+#define LCD_DATA			1	/**< Used for reading data */
+/*@}*/
+
+//-LCD-COMMANDS------------------------------------------------------------------------------------------------------
+/** \defgroup DEFINED_COMMANDS DEFINED COMMANDS
+ These defined commands should be used to configure the display. \n
+ Don't use commands from different categories together. \n
+ 
+ Configuration commands from one category should get combined to one command.
+ \par Example: 
+ \code lcd_command(LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKINGON); \endcode
+ 
+ The category modes like LCD_SHIFTMODE and LCD_CONFIGURATION can be omitted.
+*/
+/*@{*/ 
+
+/** @name GENERAL COMMANDS */
+/*@{*/ 
+#define LCD_CLEAR			0x01	/**< Clear screen */
+#define LCD_HOME			0x02	/**< Cursor move to first digit */
+/*@}*/ 
+
+/** @name ENTRYMODES */
+/*@{*/ 
+#define LCD_ENTRYMODE			0x04			/**< Set entrymode */
+	#define LCD_INCREASE		LCD_ENTRYMODE | 0x02	/**<	Set cursor move direction -- Increase */
+	#define LCD_DECREASE		LCD_ENTRYMODE | 0x00	/**<	Set cursor move direction -- Decrease */
+	#define LCD_DISPLAYSHIFTON	LCD_ENTRYMODE | 0x01	/**<	Display is shifted */
+	#define LCD_DISPLAYSHIFTOFF	LCD_ENTRYMODE | 0x00	/**<	Display is not shifted */
+/*@}*/ 
+
+/** @name DISPLAYMODES */
+/*@{*/ 
+#define LCD_DISPLAYMODE			0x08			/**< Set displaymode */
+	#define LCD_DISPLAYON		LCD_DISPLAYMODE | 0x04	/**<	Display on */
+	#define LCD_DISPLAYOFF		LCD_DISPLAYMODE | 0x00	/**<	Display off */
+	#define LCD_CURSORON		LCD_DISPLAYMODE | 0x02	/**<	Cursor on */
+	#define LCD_CURSOROFF		LCD_DISPLAYMODE | 0x00	/**<	Cursor off */
+	#define LCD_BLINKINGON		LCD_DISPLAYMODE | 0x01	/**<	Blinking on */
+	#define LCD_BLINKINGOFF		LCD_DISPLAYMODE | 0x00	/**<	Blinking off */
+/*@}*/ 
+
+/** @name SHIFTMODES */
+/*@{*/ 
+#define LCD_SHIFTMODE			0x10			/**< Set shiftmode */
+	#define LCD_DISPLAYSHIFT	LCD_SHIFTMODE | 0x08	/**<	Display shift */
+	#define LCD_CURSORMOVE		LCD_SHIFTMODE | 0x00	/**<	Cursor move */
+	#define LCD_RIGHT			LCD_SHIFTMODE | 0x04	/**<	Right shift */
+	#define LCD_LEFT			LCD_SHIFTMODE | 0x00	/**<	Left shift */
+/*@}*/ 
+
+/** @name DISPLAY_CONFIGURATION */
+/*@{*/ 
+#define LCD_CONFIGURATION		0x20				/**< Set function */
+	#define LCD_8BIT		LCD_CONFIGURATION | 0x10	/**<	8 bits interface */
+	#define LCD_4BIT		LCD_CONFIGURATION | 0x00	/**<	4 bits interface */
+	#define LCD_4LINE						    0x09	/**<	4 line display */
+	#define LCD_2LINE		LCD_CONFIGURATION | 0x08	/**<	2 line display */
+	#define LCD_1LINE		LCD_CONFIGURATION | 0x00	/**<	1 line display */
+	#define LCD_5X10		LCD_CONFIGURATION | 0x04	/**<	5 X 10 dots */
+	#define LCD_5X7			LCD_CONFIGURATION | 0x00	/**<	5 X 7 dots */
+
+#define LCD_LIGHT_OFF		LCD_LIGHT_N       // low active
+#define LCD_LIGHT_ON		0x00
+
 unsigned char i2c_start(unsigned char address)
 {
     uint8_t   twst;
@@ -90,7 +156,7 @@ unsigned char i2c_start(unsigned char address)
 
 	return 0;
 
-}/* i2c_start */
+}
 
 
 /*************************************************************************
@@ -139,7 +205,7 @@ void i2c_start_wait(unsigned char address)
     	break;
      }
 
-}/* i2c_start_wait */
+}
 
 
 /*************************************************************************
@@ -154,7 +220,7 @@ unsigned char i2c_rep_start(unsigned char address)
 {
     return i2c_start( address );
 
-}/* i2c_rep_start */
+}
 
 
 /*************************************************************************
@@ -233,28 +299,6 @@ static uint8_t lightOn=0;
 
 //-	Display initialization sequence
 
-void lcd_init(void) {
-    i2c_init();
-	
-    lcd_light(false);
-    	
-	_delay_ms(15);		        //-	Wait for more than 15ms after VDD rises to 4.5V
-	lcd_write(CMD_D1 | CMD_D0);	//-	Set interface to 8-bit
-	_delay_ms(5);			    //-	Wait for more than 4.1ms
-	lcd_write(CMD_D1 | CMD_D0);	//-	Set interface to 8-bit
-	_delay_ms(0.1);		        //-	Wait for more than 100us	
-	lcd_write(CMD_D1 | CMD_D0);	//-	Set interface to 8-bit
-	lcd_write(CMD_D1);		    //-	Set interface to 4-bit
-
-	//- From now on in 4-bit-Mode
-	lcd_command(LCD_LINE_MODE | LCD_5X7);
-	lcd_command(LCD_DISPLAYON | LCD_CURSORON | LCD_BLINKINGOFF);
-	lcd_command(LCD_CLEAR);
-    _delay_ms(2);
-	lcd_command(LCD_INCREASE | LCD_DISPLAYSHIFTOFF);
-}
-
-//-	Write data to i2c
 
 void lcd_write_i2c(uint8_t value) {
 	i2c_start_wait(LCD_I2C_DEVICE+I2C_WRITE);
@@ -264,6 +308,17 @@ void lcd_write_i2c(uint8_t value) {
 
 //-	Write nibble to display with pulse of enable bit
 // map pinout between PCF8574 and LCD
+
+//-	Read data from i2c
+
+uint8_t lcd_read_i2c(void) {
+	uint8_t lcddata = 0x00;
+
+	i2c_start_wait(LCD_I2C_DEVICE+I2C_READ);
+	lcddata = i2c_readNak();
+	i2c_stop();
+	return lcddata;
+}
 
 void lcd_write(uint8_t value) {
     uint8_t data_out=0;
@@ -281,16 +336,37 @@ void lcd_write(uint8_t value) {
 	lcd_write_i2c(data_out);	            //-	Set enable to low
 }
 
-//-	Read data from i2c
+void lcd_command(uint8_t command) {
 
-uint8_t lcd_read_i2c(void) {
-	uint8_t lcddata = 0x00;
-
-	i2c_start_wait(LCD_I2C_DEVICE+I2C_READ);
-	lcddata = i2c_readNak();
-	i2c_stop();
-	return lcddata;
+    lcd_write((command >> 4));
+	lcd_write((command & 0x0F));	
 }
+
+void lcd_init(void) {
+    // I2C init
+	TWSR = 0;
+    TWBR = 52;
+	
+    lcd_light(false);
+    	
+	_delay_ms(15);		        //-	Wait for more than 15ms after VDD rises to 4.5V
+	lcd_write(CMD_D1 | CMD_D0);	//-	Set interface to 8-bit
+	_delay_ms(5);			    //-	Wait for more than 4.1ms
+	lcd_write(CMD_D1 | CMD_D0);	//-	Set interface to 8-bit
+	_delay_ms(0.1);		        //-	Wait for more than 100us	
+	lcd_write(CMD_D1 | CMD_D0);	//-	Set interface to 8-bit
+	lcd_write(CMD_D1);		    //-	Set interface to 4-bit
+
+	//- From now on in 4-bit-Mode
+	lcd_command(LCD_LINE_MODE | LCD_5X7);
+	lcd_command(LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKINGOFF);
+	lcd_command(LCD_CLEAR);
+    _delay_ms(2);
+	lcd_command(LCD_INCREASE | LCD_DISPLAYSHIFTOFF);
+}
+
+//-	Write data to i2c
+
 
 //-	Read data from display over i2c (lower nibble contains LCD data)
 
@@ -333,195 +409,10 @@ uint8_t lcd_getbyte(bool mode)  {
 
 //-	Issue a command to the display (use the defined commands above)
 
-void lcd_command(uint8_t command) {
 
-    lcd_write((command >> 4));
-	lcd_write((command & 0x0F));	
-}
 
-//-	Print string to cursor position
 
-void lcd_print(char *string) {
-
-	while(*string)	{
-		lcd_putchar(*string++);
-	}
-}
-
-//-	Print string from flash to cursor position
-
-void lcd_print_P(PGM_P string) {
-    uint8_t c;
-
-	while((c=pgm_read_byte(string++)))	{
-		lcd_putchar(c);
-	}
-}
-
-//-	Put char to atctual cursor position
-
-void lcd_putchar(char lcddata) {
-
-	lcd_write((lcddata >> 4) | CMD_RS);
-	lcd_write((lcddata & 0x0F) | CMD_RS);
-}
-
-//-	Put char to position
-
-bool lcd_putcharlc(uint8_t line, uint8_t col, char value) {
-
-	if(!lcd_gotolc(line, col)) return false;
-	lcd_putchar(value);
-	
-	return true;
-}
-
-//-	Print string to position (If string is longer than LCD_COLS overwrite first chars)(line, row, string)
-
-bool lcd_printlc(uint8_t line, uint8_t col, char *string) {
-
-    if (!lcd_gotolc(line, col)) return false;
-    	
-	while(*string) {
-		lcd_putchar(*string++);
-		col++;
-		if(col > LCD_COLS) {
-			col = 1;
-            lcd_gotolc(line, col);
-		}
-	}
-	return true;
-}
-
-//-	Print string from flash to position
-//  (If string is longer than LCD_COLS overwrite first chars)(line, row, string)
-
-bool lcd_printlc_P(uint8_t line, uint8_t col, char *string) {
-    char c;
-    
-    if (!lcd_gotolc(line, col)) return false;
-    	
-	while((c=pgm_read_byte(string++))) {
-		lcd_putchar(c);
-		col++;
-		if(col > LCD_COLS) {
-			col = 1;
-            lcd_gotolc(line, col);
-		}
-	}
-	return true;
-}
-
-//-	Print string to position (If string is longer than LCD_COLS continue in next line)
-
-bool lcd_printlcc(uint8_t line, uint8_t col, char *string) {	
-    
-    if (!lcd_gotolc(line, col)) return false;
-    	
-	while(*string) {
-		lcd_putchar(*string++);
-		col++;
-		if(col > LCD_COLS) {
-			line++;
-			col = 1; 
-    		if(line > LCD_LINES) {
-			    line = 1;
-		    }
-            lcd_gotolc(line, col);
-		}
-	}
-	return true;
-}
-
-//-	Print string from flash to position
-// (If string is longer than LCD_COLS continue in next line)
-
-bool lcd_printlcc_P(uint8_t line, uint8_t col, char *string) {	
-    char c;
-      
-    if (!lcd_gotolc(line, col)) return false;
-    	
-	while((c=pgm_read_byte(string++))) {
-		lcd_putchar(c);
-		col++;
-		if(col > LCD_COLS) {
-			line++;
-			col = 1; 
-    		if(line > LCD_LINES) {
-			    line = 1;
-		    }
-            lcd_gotolc(line, col);
-		}
-	}
-	return true;
-}
-
-//-	Go to position (line, column)
-
-bool lcd_gotolc(uint8_t line, uint8_t col ) {
-	uint8_t lcddata=0;
-	
-	if( (line > LCD_LINES) ||
-	    (col > LCD_COLS) ||
-	    ((line == 0) || (col == 0)) ) return false;
-	
-    switch (line) {
-        case 1: lcddata = LCD_LINE1; break;
-        case 2: lcddata = LCD_LINE2; break;
-        //case 3: lcddata = LCD_LINE3; break;
-       // case 4: lcddata = LCD_LINE4; break;
-    }
-    lcddata |= 0x80;
-    lcddata += (col-1);
-	lcd_command(lcddata);
-	return true;	
-}
-
-//-	Go to nextline (if next line > (LCD_LINES-1) return false)
-
-bool lcd_nextline(void) {
-	uint8_t line, col;
-
-	lcd_getlc(&line, &col);
-	if (!lcd_gotolc(line + 1, 1)) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-//-	Get line and row (target byte for line, target byte for row)
-
-bool lcd_getlc(uint8_t *line, uint8_t *col) {
-	uint8_t lcddata;
-
-	lcddata = lcd_getbyte(LCD_ADDRESS);
-	if (lcddata & (1 << 7)) return false;       // LCD busy
-
-    if (lcddata >= LCD_LINE1 && lcddata < (LCD_LINE1+LCD_COLS)) {
-        *line = 1;
-        *col = lcddata - LCD_LINE1 + 1;
-        return true;
-    } else if (lcddata >= LCD_LINE2 && lcddata < (LCD_LINE2+LCD_COLS)) {
-        *line = 2;
-        *col = lcddata - LCD_LINE2 + 1;
-        return true;
-    } /*else if (lcddata >= LCD_LINE3 && lcddata < (LCD_LINE3+LCD_COLS)) {
-        *line = 3;
-        *col = lcddata - LCD_LINE3 + 1;
-        return true;
-    } else if (lcddata >= LCD_LINE4 && lcddata < (LCD_LINE4+LCD_COLS)) {
-        *line = 4;
-        *col = lcddata - LCD_LINE4 + 1;
-        return true;
-    }*/
-
-	return false;
-}
-
-// turn light on/off
-
-void lcd_light(bool light) {
+void lcd_light(uint8_t light) {
 
     if (!light) {
         lightOn = 1;
@@ -532,15 +423,37 @@ void lcd_light(bool light) {
     }
 }
 
-//-	Check if busy
 
-bool lcd_busy(void) {
-	uint8_t state;
-    
-    state = lcd_getbyte(LCD_ADDRESS);
-	if (state & (1 << 7)) {
-        return true;
-    } else {
-        return false;
-    }
+#include <stdio.h>
+
+void lcd_printf(uint8_t line, uint8_t pos, char *msg, ...) {
+	char buffer[16];
+	va_list args;
+
+  	va_start (args, msg);
+	vsnprintf(buffer, 16, msg, args);
+	va_end(args);
+
+	if (line > 1 || pos > 15) {
+		return;
+	}
+
+	uint8_t lcddata = 0x80;
+	
+	if (line == 1) {
+		lcddata |= 0x40;
+	}
+
+    lcddata |= 0x80;
+    lcddata += pos - 1;
+	lcd_command(lcddata);
+
+	uint8_t index = 0;
+
+	while (pos < 16 && buffer[index]) {
+		lcd_write((buffer[index] >> 4) | CMD_RS);
+		lcd_write((buffer[index] & 0x0F) | CMD_RS);
+		pos++;
+		index++;
+	}
 }
